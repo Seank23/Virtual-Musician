0f1cdd862c5dbaf4deb64d45a96e42ad40d36981
DESKTOP-NFES7CF\seank <sean.king@gmx.com>
2017-08-01 22:52:58 +0100

    v0.0.2 - Setup Main Menu Animation & Options Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,53 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject mainMenuPanel;
-    public GameObject newPlayerPanel;
-    public GameObject optionsPanel;
-    private GameObject activeMenu;
+    public Menu currentMenu;
 
-    void Start()
+    public void Start()
     {
-        mainMenuPanel.SetActive(true);
-        newPlayerPanel.SetActive(false);
-        //optionsPanel.SetActive(false);
+        ShowMenu(currentMenu);
     }
 
-    public void GoToMenu(string menuName)
+    public void ShowMenu(Menu menu)
     {
-        switch (menuName)
-        {
-            case "MainMenu":
-                mainMenuPanel.SetActive(true);
-                break;
+        if (currentMenu != null)
+            currentMenu.IsOpen = false;
 
-            case "NewPlayer":
-                activeMenu = mainMenuPanel;
-                newPlayerPanel.transform.Translate(-1500, 0, 0);
-                StartCoroutine(SlideMenu(new Vector3(1500, 0, 0), 1));
-                //mainMenuPanel.SetActive(false);
-                //newPlayerPanel.SetActive(true);
-
-                break;
-
-            case "Options":
-                break;
-        }
-    }
-
-    IEnumerator SlideMenu(Vector3 translation, int time)
-    {
-        for(float t = 0.0f; t < 1.0f; t += Time.deltaTime / time)
-        {
-            Vector3 frame = new Vector3(Mathf.Lerp(0, translation.x, t),
-                                        Mathf.Lerp(0, translation.y, t),
-                                        Mathf.Lerp(0, translation.z, t));
-            print(frame.x);
-        }
-        yield return null;
+        currentMenu = menu;
+        currentMenu.IsOpen = true;
     }
 }
---
+++ Assets/Scripts/TabSelector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TabSelector : MonoBehaviour
+{
+    public GameObject[] tabs;
+    public GameObject[] menus;
+    private Color defaultColor;
+    public float alpha = 0.8f;
+
+    void Awake()
+    {
+        defaultColor = tabs[0].GetComponent<Image>().color;
+
+        EventSystem.current.SetSelectedGameObject(tabs[0]);
+        SetActiveMenu(menus[0]);
+    }
+
+    public void SetActiveMenu(GameObject menuToBeSet)
+    {
+        foreach (GameObject menu in menus)
+        {
+            menu.SetActive(false);
+        }
+
+        foreach(GameObject tab in tabs)
+        {
+            tab.GetComponent<Image>().color = defaultColor;
+        }
+
+        menuToBeSet.SetActive(true);
+        //Color menuColor = menuToBeSet.GetComponent<Image>().color;
+        //menuColor.a = alpha;
+        //menuToBeSet.GetComponent<Image>().color = menuColor;
+
+        GameObject currentTab = EventSystem.current.currentSelectedGameObject;
+        Color tabColor = currentTab.GetComponent<Image>().color;
+        tabColor.a = alpha;
+        currentTab.GetComponent<Image>().color = tabColor;
+    }
+}
